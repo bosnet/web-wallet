ec98a041b1cfcee06434d7141ef8f7b2a1323749
MoonKim <forcas7274@ermind.co.kr>
2018-11-16 10:21:18 +0900

    Fix Issue #1

--- src/components/AmountInput.js
+++ src/components/AmountInput.js
@@ -1,66 +1,80 @@
 import React, { Component } from 'react';
 
 class AmountInput extends Component {
-	onKeyDown = ( $event ) => {
-		const key = $event.key;
+    constructor() {
+        super();
 
-		const map = [
-      '.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-			'ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown',
-			'Backspace', 'Home', 'End', 'Delete'];
+        const state = {
+            text: '',
+        };
 
+        this.state = state;
+    }
 
-		if ( map.indexOf( key ) === -1 || (key === '.' && $event.currentTarget.value.match(/[.]/)) ) {
-			$event.preventDefault();
-		}
+    onKeyDown = ($event) => {
+        const key = $event.key;
 
-		const MAX_DECIMAL_LENGTH = 7;
-		const NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-		
-		if(NUMBERS.indexOf( key ) !== -1)
-		{
-			var input = $event.currentTarget.value;
-			if(input.indexOf(".") !== -1)
-			{
-				var decimal = input.substr(input.indexOf(".") +1);
-				
-				if(decimal.length >= MAX_DECIMAL_LENGTH)
-				{
-					$event.preventDefault();
-				}
-			}
-		}
-	};
-
-	onKeyUp = ( $event ) => {
-		let value = $event.currentTarget.value;
-
-		if(value.length >= 2)
-		{
-			var first = value.charAt(0);
-			var second = value.charAt(1);
-
-			if(first === '0' && second !== '.')
-			{
-				value = value.substr(1);
-			}
-		}
-
-		$event.currentTarget.value = value;
-	};
+        const map = [
+            '.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+            'ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown',
+            'Backspace', 'Home', 'End', 'Delete'];
 
 
-	render() {
-		return (
-			<input {...this.props}
-				   onKeyDown={this.onKeyDown}
-					 onKeyUp={this.onKeyUp}
-					 onBlur={this.onKeyUp}
-				   type="number"
-				   min="0.1"
-				   placeholder="0.1"/>
-		)
-	}
+        if (map.indexOf(key) === -1 || (key === '.' && $event.currentTarget.value.match(/[.]/))) {
+            $event.preventDefault();
+        }
+
+        const MAX_DECIMAL_LENGTH = 7;
+        const NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+        if (NUMBERS.indexOf(key) !== -1) {
+            const input = $event.currentTarget.value;
+            if (input.indexOf(".") !== -1) {
+                const decimal = input.substr(input.indexOf(".") + 1);
+
+                if (decimal.length >= MAX_DECIMAL_LENGTH) {
+                    $event.preventDefault();
+                }
+            }
+        }
+    };
+
+    onKeyUp = ($event) => {
+        let value = $event.currentTarget.value;
+
+        if (value.length >= 2) {
+            const first = value.charAt(0);
+            const second = value.charAt(1);
+
+            if (first === '0' && second !== '.') {
+                value = value.substr(1);
+            }
+        }
+
+
+        if (!$event.currentTarget.validity.badInput) {
+            this.setState({
+                text: value
+            });
+            $event.currentTarget.value = value;
+        }
+        else {
+            $event.currentTarget.value = this.state.text;
+        }
+    };
+
+
+    render() {
+        return (
+            <input {...this.props}
+                onKeyDown={this.onKeyDown}
+                onKeyUp={this.onKeyUp}
+                onBlur={this.onKeyUp}
+                type="number"
+                min="0.1"
+                placeholder="0.1" />
+        )
+    }
 }
 
 export default AmountInput;
